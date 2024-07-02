dfa51129b2a411396abc837ddabfdc22e6dab231
Hevin Jantasmin <hjantasmin@bangkit.com>
2024-07-02 10:01:57 -0700

    refetched the grand total under Cart nav button when changing quantity on Cart page

--- assets/js/theme/cart.js
+++ assets/js/theme/cart.js
@@ -30,6 +30,25 @@ export default class Cart extends PageManager {
         }
     }
 
+    fetchCartGrandTotal() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/cart.php', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(xhr.responseText, 'text/html');
+                    var grandTotalElement = doc.querySelector('.cart-total-grandTotal span');
+                    var grand_total = grandTotalElement ? grandTotalElement.textContent : "$0.00";
+                    document.getElementById('custom-total-under-cart').innerHTML = grand_total;
+                } else {
+                    document.getElementById('custom-total-under-cart').innerHTML = '$0.00';
+                }
+            }
+        };
+        xhr.send();
+    };
+
     cartUpdate($target) {
         const itemId = $target.data('cartItemid');
         this.$activeCartItemId = itemId;
@@ -64,6 +83,8 @@ export default class Cart extends PageManager {
                 showAlertModal(response.data.errors.join('\n'));
             }
         });
+
+        fetchCartGrandTotal();
     }
 
     cartUpdateQtyTextChange($target, preVal = null) {
@@ -105,6 +126,8 @@ export default class Cart extends PageManager {
                 return showAlertModal(response.data.errors.join('\n'));
             }
         });
+
+        fetchCartGrandTotal();
     }
 
     cartRemoveItem(itemId) {
